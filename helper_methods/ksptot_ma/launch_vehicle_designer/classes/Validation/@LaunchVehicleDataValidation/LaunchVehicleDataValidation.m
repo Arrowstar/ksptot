d8a0c3664cdffec4ba1b8d3bcdd3d31e3b02891c
Arrowstar <hardena@alumni.msoe.edu>
2019-01-04 14:14:47 -0600

    LVD: Added validator to check if drag model is active when in an atmosphere

--- helper_methods/ksptot_ma/launch_vehicle_designer/classes/Validation/@LaunchVehicleDataValidation/LaunchVehicleDataValidation.m
+++ helper_methods/ksptot_ma/launch_vehicle_designer/classes/Validation/@LaunchVehicleDataValidation/LaunchVehicleDataValidation.m
@@ -19,6 +19,7 @@ classdef LaunchVehicleDataValidation < matlab.mixin.SetGet
             obj.validators(end+1) = MaxSimTimeReachedValidator(obj.lvdData);
             obj.validators(end+1) = MaxPropTimeReachedValidator(obj.lvdData);
             obj.validators(end+1) = ThrottleWithNoThrustModelValidator(obj.lvdData);
+            obj.validators(end+1) = AtmoWithNoDragModelValidator(obj.lvdData);
         end
 
         function validate(obj)
