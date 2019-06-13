c9c28e1156cd08257e5a8ce1d11afd06b17c58c2
Arrowstar <hardena@alumni.msoe.edu>
2019-06-13 18:02:18 -0500

    Updated version string to 1.6.4

--- helper_methods/getKSPTOTVersionNumStr.m
+++ helper_methods/getKSPTOTVersionNumStr.m
@@ -1,6 +1,6 @@
 function [verNumStr] = getKSPTOTVersionNumStr()
 %getKSPTOTVersionNumStr Summary of this function goes here
 %   Detailed explanation goes here
-    verNumStr = '1.6.3';
+    verNumStr = '1.6.4';
 end
 
