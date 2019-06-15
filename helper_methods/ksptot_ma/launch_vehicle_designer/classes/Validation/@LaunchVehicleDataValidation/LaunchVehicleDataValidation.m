f389364687f1659f5027ef0bf96425d36a07e529
Arrowstar <hardena@alumni.msoe.edu>
2019-06-15 11:55:53 -0500

    LVD: Added radius beyond SoI radius validator.

--- helper_methods/ksptot_ma/launch_vehicle_designer/classes/Validation/@LaunchVehicleDataValidation/LaunchVehicleDataValidation.m
+++ helper_methods/ksptot_ma/launch_vehicle_designer/classes/Validation/@LaunchVehicleDataValidation/LaunchVehicleDataValidation.m
@@ -21,6 +21,7 @@ classdef LaunchVehicleDataValidation < matlab.mixin.SetGet
             obj.validators(end+1) = ThrottleWithNoThrustModelValidator(obj.lvdData);
             obj.validators(end+1) = AtmoWithNoDragModelValidator(obj.lvdData);
             obj.validators(end+1) = MinAltitudeReachedValidator(obj.lvdData);
+            obj.validators(end+1) = RadiusOutsideSoIValidator(obj.lvdData);
         end
 
         function validate(obj)
