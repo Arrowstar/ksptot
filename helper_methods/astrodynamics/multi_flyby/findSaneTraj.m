4cddad51e9785f92d19b48f9ae97005a8e711f7d
Arrowstar <hardena@alumni.msoe.edu>
2020-02-21 18:56:13 -0600

    MFMS should now handle local body frames which are not aligned to the parent frame.

--- helper_methods/astrodynamics/multi_flyby/findSaneTraj.m
+++ helper_methods/astrodynamics/multi_flyby/findSaneTraj.m
@@ -28,7 +28,7 @@ function [x, dv, rp, orbitsIn, orbitsOut, deltaVVect, vInfDNorm, xferOrbits, c, 
     try
         [x3,dv3] = fmincon(objfun,x0,[],[],[],[],lb,ub,nonlconFMC1, optionsFMC);
     catch ME %#ok<NASGU>
-        x3 = x2;
+        x3 = x2(1:length(bodiesInfo));
         dv3 = dv2;
     end
     x3 = [x3,tm,numRevInds];
@@ -48,7 +48,39 @@ function [x, dv, rp, orbitsIn, orbitsOut, deltaVVect, vInfDNorm, xferOrbits, c, 
 %         end
 %     end
     
-    [dv, rp, orbitsIn, orbitsOut, deltaVVect, vInfDNorm, vInfDepart, vInfArrive, ~, r1B, r2B, v1, v2] = fitnessfcn(x);
+    [dv, rp, orbitsIn, orbitsOut, deltaVVect, vInfDNorm, ~, ~, ~, r1B, r2B, v1, v2] = fitnessfcn(x);
+    
+    times = cumsum(x3(1:length(bodiesInfo)));
+    parentBodyFrame = bodiesInfo{1}.getParBodyInfo().getBodyCenteredInertialFrame();
+    for(i=1:size(orbitsIn,1))
+        time = times(1+i);
+        bodyFrame = flybyBodies{i}.getBodyCenteredInertialFrame();
+        
+        cartStateParent = CartesianElementSet(time, r1B(:,i+1), v1(:,i+1), parentBodyFrame);
+        vInfOut = cartStateParent.convertToFrame(bodyFrame).vVect;
+        
+        cartStateParent = CartesianElementSet(time, r2B(:,i), v2(:,i), parentBodyFrame);
+        vInfIn = cartStateParent.convertToFrame(bodyFrame).vVect;
+        
+        [smaIn, eIn, hHat, sHat, smaOut, eOut, oHat] = computeMultiFlyByParameters(vInfIn, vInfOut, flybyBodies{i}.gm);
+        [hSMAIn, hEccIn, hIncIn, hRAANIn, hArgIn, ~, ~, ~] = computeHyperOrbitFromMultiFlybyParams(smaIn, eIn, hHat, sHat, true);
+        [hSMAOut, hEccOut, hIncOut, hRAANOut, hArgOut, ~, ~, ~] = computeHyperOrbitFromMultiFlybyParams(smaOut, eOut, hHat, oHat, false);
+        
+        orbitsIn(i,:) = [hSMAIn', hEccIn', hIncIn', hRAANIn', hArgIn', zeros(size(hSMAIn')), ((1-eIn).*smaIn)'];
+        orbitsOut(i,:) = [hSMAOut', hEccOut', hIncOut', hRAANOut', hArgOut', zeros(size(hSMAOut')), ((1-eOut).*smaOut)'];
+        
+        inOrbitKepState = KeplerianElementSet(time, hSMAIn', hEccIn', hIncIn', hRAANIn', hArgIn', 0, bodyFrame);
+        outOrbitKepState = KeplerianElementSet(time, hSMAOut', hEccOut', hIncOut', hRAANOut', hArgOut', 0, bodyFrame);
+        
+        deltaVVect(:,1) = outOrbitKepState.convertToCartesianElementSet().vVect - inOrbitKepState.convertToCartesianElementSet().vVect;
+    end
+    
+    cartStateParent = CartesianElementSet(times(1), r1B(:,1), v1(:,1), parentBodyFrame);
+    vInfDepart = cartStateParent.convertToFrame(bodiesInfo{1}.getBodyCenteredInertialFrame()).vVect;
+    
+    cartStateParent = CartesianElementSet(times(end), r1B(:,end), v2(:,end), parentBodyFrame);
+    vInfArrive = cartStateParent.convertToFrame(bodiesInfo{end}.getBodyCenteredInertialFrame()).vVect;
+    
     xferOrbits = getMultiFlybyXferOrbits(x, numRev, r1B, r2B, v1, v2, bodiesInfo, celBodyData);
     c = nonlconFMC2(x);  
 end
