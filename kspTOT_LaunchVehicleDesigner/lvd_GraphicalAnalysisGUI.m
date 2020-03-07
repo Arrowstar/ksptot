fd1336bb7da6842297356b1e21cd2676c71d4f6c
Arrowstar <hardena@alumni.msoe.edu>
2020-03-07 15:00:07 -0600

    LVD: Fixed GA periapsis crossing marker line issue that was also fixed for MA's GA

--- kspTOT_LaunchVehicleDesigner/lvd_GraphicalAnalysisGUI.m
+++ kspTOT_LaunchVehicleDesigner/lvd_GraphicalAnalysisGUI.m
@@ -700,7 +700,8 @@ function plotData(hFig, indepVarValues, data, lineColor, lineType, lineWidth, in
                 tru1 = tru(j-1);
                 tru2 = tru(j);
                 
-                if(((tru1 > tru2 && abs(tru1-tru2)>pi) || (tru1 < 0 && tru2 >= 0)) && bodyLog(j-1,8)==bodyLog(j,8))                   
+%                 if(((tru1 > tru2 && abs(tru1-tru2)>pi) || (tru1 < 0 && tru2 >= 0)) && bodyLog(j-1,8)==bodyLog(j,8))  
+                if(((tru1 < 0 && tru2 >= 0)) && bodyLog(j-1,8)==bodyLog(j,8)) 
                     eventTime = bodyLog(j,1)*indepTimeUnitMult;
                     indepVarEventLoc = indepVarValues(indepVarValues(:,2)==eventTime,1);
                     indepVarEventLoc = indepVarEventLoc(1);
