cb1123174f778e7dced766dc288469ec09f1683b
Arrowstar <hardena@alumni.msoe.edu>
2020-08-16 11:05:06 -0500

    LVD: Update version number to 1.6.6 (final)

--- helper_methods/getKSPTOTVersionNumStr.m
+++ helper_methods/getKSPTOTVersionNumStr.m
@@ -1,6 +1,6 @@
 function [verNumStr] = getKSPTOTVersionNumStr()
 %getKSPTOTVersionNumStr Summary of this function goes here
 %   Detailed explanation goes here
-    verNumStr = '1.6.6 PR11';
+    verNumStr = '1.6.6';
 end
 
