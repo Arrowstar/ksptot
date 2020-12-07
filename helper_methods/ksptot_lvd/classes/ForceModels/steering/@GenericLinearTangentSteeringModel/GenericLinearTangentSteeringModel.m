7f3b438c3cf3711b0b76bbfff735955a57122b8c
Arrowstar <hardena@alumni.msoe.edu>
2020-12-06 19:38:05 -0600

    Forgot to add

---
+++ helper_methods/ksptot_lvd/classes/ForceModels/steering/@GenericLinearTangentSteeringModel/GenericLinearTangentSteeringModel.m
@@ -0,0 +1,184 @@
+classdef GenericLinearTangentSteeringModel < AbstractAnglePolySteeringModel
+    %GenericLinearTangentSteeringModel Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        gammaAngleModel(1,1) PolynominalModel = PolynominalModel(0,0,0,0);
+        betaAngleModel(1,1) LinearTangentModel = LinearTangentModel(0,0,0,0,0);
+        alphaAngleModel(1,1) PolynominalModel = PolynominalModel(0,0,0,0);
+        
+        gammaContinuity(1,1) logical = false;
+        betaContinuity(1,1) logical = false;
+        alphaContinuity(1,1) logical = false;
+        
+        refFrame AbstractReferenceFrame
+        controlFrame AbstractControlFrame
+    end
+    
+    methods       
+        function dcm = getBody2InertialDcmAtTime(obj, ut, rVect, vVect, bodyInfo)
+            gammaAng = obj.gammaAngleModel.getValueAtTime(ut);
+            betaAng = obj.betaAngleModel.getValueAtTime(ut);
+            alphaAng = obj.alphaAngleModel.getValueAtTime(ut);
+            
+            elemSet = CartesianElementSet(ut, rVect(:), vVect(:), bodyInfo.getBodyCenteredInertialFrame());
+            elemSet = elemSet.convertToFrame(obj.refFrame);
+            
+            dcm = obj.controlFrame.computeDcmToInertialFrame(elemSet.time, elemSet.rVect, elemSet.vVect, elemSet.frame.getOriginBody(), gammaAng, betaAng, alphaAng, obj.refFrame);
+        end
+
+        function [angleModel, continuity] = getAngleNModel(obj, n)
+            angleModel = PolynominalModel.empty(1,0);
+            
+            switch n
+                case 1
+                    angleModel = obj.gammaAngleModel;
+                    continuity = obj.gammaContinuity;
+                case 2
+                    angleModel = obj.betaAngleModel;
+                    continuity = obj.betaContinuity;
+                case 3
+                    angleModel = obj.alphaAngleModel;
+                    continuity = obj.alphaContinuity;
+            end
+        end
+        
+        function setT0(obj, newT0)
+            obj.gammaAngleModel.t0 = newT0;
+            obj.betaAngleModel.t0 = newT0;
+            obj.alphaAngleModel.t0 = newT0;
+        end
+        
+        function setConstTerms(obj, gammaConst, alphaConst)
+            obj.gammaAngleModel.constTerm = gammaConst;
+            obj.alphaAngleModel.constTerm = alphaConst;
+        end
+        
+        function setLinearTerms(obj, gammaRate, alphaRate)
+            obj.gammaAngleModel.linearTerm = gammaRate;
+            obj.alphaAngleModel.linearTerm = alphaRate;
+        end
+        
+        function setAccelTerms(obj, gammaRateRate, alphaRateRate)
+            obj.gammaAngleModel.accelTerm = gammaRateRate;
+            obj.alphaAngleModel.accelTerm = alphaRateRate;
+        end
+        
+        function setLinearTangentTerms(obj, a, a_dot, b, b_dot)
+            obj.betaAngleModel.a = a;
+            obj.betaAngleModel.a_dot = a_dot;
+            obj.betaAngleModel.b = b;
+            obj.betaAngleModel.b_dot = b_dot;
+        end
+        
+        function [angle1Cont, angle2Cont, angle3Cont] = getContinuityTerms(obj)
+            angle1Cont = obj.gammaContinuity;
+            angle2Cont = obj.betaContinuity;
+            angle3Cont = obj.alphaContinuity;
+        end
+        
+        function setContinuityTerms(obj, angle1Cont, angle2Cont, angle3Cont)
+            obj.gammaContinuity = angle1Cont;
+            obj.betaContinuity = angle2Cont;
+            obj.alphaContinuity = angle3Cont;
+        end
+        
+        function setConstsFromDcmAndContinuitySettings(obj, dcm, ut, rVect, vVect, bodyInfo)
+            if(obj.gammaContinuity || obj.betaContinuity || obj.alphaContinuity)
+                elemSet = CartesianElementSet(ut, rVect(:), vVect(:), bodyInfo.getBodyCenteredInertialFrame());
+                elemSet = elemSet.convertToFrame(obj.refFrame);
+                
+                [gammaAngle, betaAngle, alphaAngle] = obj.controlFrame.getAnglesFromInertialBodyAxes(dcm, elemSet.time, elemSet.rVect(:), elemSet.vVect(:), elemSet.frame.getOriginBody(), obj.refFrame);
+             
+                if(obj.gammaContinuity)
+                    obj.gammaAngleModel.constTerm = gammaAngle;
+                end
+                
+                if(obj.betaContinuity)
+                    obj.betaAngleModel.setBForContinuity(betaAngle);
+                end
+                
+                if(obj.alphaContinuity)
+                    obj.alphaAngleModel.constTerm = alphaAngle;
+                end
+            end
+        end
+        
+        function [angle1Name, angle2Name, angle3Name] = getAngleNames(~)
+            angle1Name = 'Gamma';
+            angle2Name = 'Beta';
+            angle3Name = 'Alpha';
+        end
+        
+        function newSteeringModel = deepCopy(obj)
+            newSteeringModel = GenericPolySteeringModel(obj.gammaAngleModel.deepCopy(), obj.betaAngleModel.deepCopy(), obj.alphaAngleModel.deepCopy());
+            newSteeringModel.gammaContinuity = obj.gammaContinuity;
+            newSteeringModel.betaContinuity = obj.betaContinuity;
+            newSteeringModel.alphaContinuity = obj.alphaContinuity;
+            newSteeringModel.refFrame = obj.refFrame;
+            newSteeringModel.controlFrame = obj.controlFrame;
+        end
+        
+        function optVar = getNewOptVar(obj)
+            optVar = SetGenericLinearTangentSteeringModelActionOptimVar(obj);
+        end
+        
+        function optVar = getExistingOptVar(obj)
+            optVar = obj.optVar;
+        end
+        
+        function tf = usesRefFrame(~)
+            tf = true;
+        end
+        
+        function refFrame = getRefFrame(obj)
+            refFrame = obj.refFrame;
+        end
+        
+        function setRefFrame(obj, refFrame)
+            obj.refFrame = refFrame;
+        end
+        
+        function tf = usesControlFrame(~)
+            tf = true;
+        end
+        
+        function cFrame = getControlFrame(obj)
+            cFrame = obj.controlFrame;
+        end
+        
+        function setControlFrame(obj, cFrame)
+            obj.controlFrame = cFrame;
+        end
+        
+        function tf = requiresReInitAfterSoIChange(obj)
+            tf = false;
+        end
+        
+        function enum = getSteeringModelTypeEnum(~)
+            enum = SteerModelTypeEnum.LinearTangentAngles;
+        end
+    end
+    
+    methods(Access=private)
+        function obj = GenericLinearTangentSteeringModel(gammaAngleModel, betaAngleModel, alphaAngleModel)
+            obj.gammaAngleModel = gammaAngleModel;
+            obj.betaAngleModel = betaAngleModel;
+            obj.alphaAngleModel = alphaAngleModel;
+        end        
+    end
+    
+    methods(Static)
+        function model = getDefaultSteeringModel()
+            gammaAngleModel = PolynominalModel(0,0,0,0);
+            betaAngleModel = LinearTangentModel(0,0,0,0,0);
+            alphaAngleModel = PolynominalModel(0,0,0,0);
+            
+            model = GenericLinearTangentSteeringModel(gammaAngleModel, betaAngleModel, alphaAngleModel);
+        end
+        
+        function typeStr = getTypeNameStr()
+            typeStr = SteeringModelEnum.GenericLinTan.nameStr;
+        end
+    end
+end
---
+++ helper_methods/ksptot_lvd/classes/Optimization/variables/@SetGenericLinearTangentSteeringModelActionOptimVar/SetGenericLinearTangentSteeringModelActionOptimVar.m
@@ -0,0 +1,185 @@
+classdef SetGenericLinearTangentSteeringModelActionOptimVar < AbstractOptimizationVariable
+    %SetGenericLinearTangentSteeringModelActionOptimVar Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        varObj = GenericLinearTangentSteeringModel.getDefaultSteeringModel();
+        
+        lb(1,10) double
+        ub(1,10) double
+        
+        varGammaConst(1,1) logical = false;
+        varGammaLin(1,1) logical   = false;
+        varGammaAccel(1,1) logical = false;
+        
+        varBetaA(1,1) logical = false;
+        varBetaADot(1,1) logical = false;
+        varBetaB(1,1) logical = false;
+        varBetaBDot(1,1) logical = false;
+        
+        varAlphaConst(1,1) logical = false;
+        varAlphaLin(1,1) logical   = false;
+        varAlphaAccel(1,1) logical = false;
+    end
+    
+    methods
+        function obj = SetGenericLinearTangentSteeringModelActionOptimVar(varObj)
+            obj.varObj = varObj;
+            obj.varObj.optVar = obj;
+            
+            obj.id = rand();
+        end
+        
+        function x = getXsForVariable(obj)
+            x = NaN(1,10);
+            
+            if(obj.varGammaConst)
+                x(1) = obj.varObj.gammaAngleModel.constTerm;
+            end
+            if(obj.varGammaLin)
+                x(2) = obj.varObj.gammaAngleModel.linearTerm;
+            end
+            if(obj.varGammaAccel)
+                x(3) = obj.varObj.gammaAngleModel.accelTerm;
+            end
+            
+            if(obj.varBetaA)
+                x(4) = obj.varObj.betaAngleModel.a;
+            end
+            if(obj.varBetaADot)
+                x(5) = obj.varObj.betaAngleModel.a_dot;
+            end
+            if(obj.varBetaB)
+                x(6) = obj.varObj.betaAngleModel.b;
+            end
+            if(obj.varBetaBDot)
+                x(7) = obj.varObj.betaAngleModel.b_dot;
+            end
+            
+            if(obj.varAlphaConst)
+                x(8) = obj.varObj.alphaAngleModel.constTerm;
+            end
+            if(obj.varAlphaLin)
+                x(9) = obj.varObj.alphaAngleModel.linearTerm;
+            end
+            if(obj.varAlphaAccel)
+                x(10) = obj.varObj.alphaAngleModel.accelTerm;
+            end
+            
+            x(isnan(x)) = [];
+        end
+        
+        function [lb, ub] = getBndsForVariable(obj)            
+            useTf = obj.getUseTfForVariable();
+
+            lb = obj.lb(useTf);
+            ub = obj.ub(useTf);
+        end
+        
+        function [lb, ub] = getAllBndsForVariable(obj)
+            lb = obj.lb;
+            ub = obj.lb;
+        end
+        
+        function setBndsForVariable(obj, lb, ub)
+            if(length(lb) == 10 && length(ub) == 10)
+                obj.lb = lb;
+                obj.ub = ub;
+            else
+                useTf = obj.getUseTfForVariable();
+
+                obj.lb(useTf) = lb;
+                obj.ub(useTf) = ub;
+            end
+        end
+        
+        function useTf = getUseTfForVariable(obj)
+            useTf = [obj.varGammaConst obj.varGammaLin obj.varGammaAccel ...
+                     obj.varBetaA obj.varBetaADot obj.varBetaB obj.varBetaBDot ...
+                     obj.varAlphaConst obj.varAlphaLin obj.varAlphaAccel];
+        end
+        
+        function setUseTfForVariable(obj, useTf)                 
+            obj.varGammaConst = useTf(1);
+            obj.varGammaLin = useTf(2);
+            obj.varGammaAccel = useTf(3);
+            
+            obj.varBetaA = useTf(4);
+            obj.varBetaADot = useTf(5);
+            obj.varBetaB = useTf(6);
+            obj.varBetaBDot = useTf(7);
+            
+            obj.varAlphaConst = useTf(8);
+            obj.varAlphaLin = useTf(9);
+            obj.varAlphaAccel = useTf(10);
+        end
+        
+        function updateObjWithVarValue(obj, x)
+            ind = 1;
+            
+            if(obj.varGammaConst)
+                obj.varObj.gammaAngleModel.constTerm = x(ind);
+                ind = ind + 1;
+            end
+            if(obj.varGammaLin)
+                obj.varObj.gammaAngleModel.linearTerm = x(ind);
+                ind = ind + 1;
+            end
+            if(obj.varGammaAccel)
+                obj.varObj.gammaAngleModel.accelTerm = x(ind);
+                ind = ind + 1;
+            end
+            
+            if(obj.varBetaA)
+                obj.varObj.betaAngleModel.a = x(ind);
+                ind = ind + 1;
+            end
+            if(obj.varBetaADot)
+                obj.varObj.betaAngleModel.a_dot = x(ind);
+                ind = ind + 1;
+            end
+            if(obj.varBetaB)
+                obj.varObj.betaAngleModel.b = x(ind);
+                ind = ind + 1;
+            end
+            if(obj.varBetaBDot)
+                obj.varObj.betaAngleModel.b_dot = x(ind);
+                ind = ind + 1;
+            end
+            
+            if(obj.varAlphaConst)
+                obj.varObj.alphaAngleModel.constTerm = x(ind);
+                ind = ind + 1;
+            end
+            if(obj.varAlphaLin)
+                obj.varObj.alphaAngleModel.linearTerm = x(ind);
+                ind = ind + 1;
+            end
+            if(obj.varAlphaAccel)
+                obj.varObj.alphaAngleModel.accelTerm = x(ind);
+                ind = ind + 1; %#ok<NASGU>
+            end
+        end
+        
+        function nameStrs = getStrNamesOfVars(obj, evtNum, varLocType)
+           if(evtNum > 0)
+                subStr = sprintf('Event %i',evtNum);
+            else
+                subStr = varLocType;
+            end
+            
+            nameStrs = {sprintf('%s Gamma Angle Constant', subStr), ...
+                        sprintf('%s Gamma Angle Rate', subStr), ...
+                        sprintf('%s Gamma Angle Acceleration', subStr), ...
+                        sprintf('%s Beta Angle Linear Tangent A', subStr), ...
+                        sprintf('%s Beta Angle Linear Tangent A Rate', subStr), ...
+                        sprintf('%s Beta Angle Linear Tangent B', subStr), ...
+                        sprintf('%s Beta Angle Linear Tangent B Rate', subStr), ...
+                        sprintf('%s Alpha Angle Constant', subStr), ...
+                        sprintf('%s Alpha Angle Rate', subStr), ...
+                        sprintf('%s Alpha Angle Acceleration', subStr)};
+                    
+            nameStrs = nameStrs(obj.getUseTfForVariable());
+        end
+    end
+end
