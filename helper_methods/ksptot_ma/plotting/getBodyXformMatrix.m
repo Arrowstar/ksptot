7e5e1a186bb2edd4383a8488820e76ed62aa431f
Arrowstar <hardena@alumni.msoe.edu>
2020-12-14 14:54:33 -0600

    Removed dependence on rotz

--- helper_methods/ksptot_ma/plotting/getBodyXformMatrix.m
+++ helper_methods/ksptot_ma/plotting/getBodyXformMatrix.m
@@ -5,7 +5,10 @@ function M = getBodyXformMatrix(time, bodyInfo, viewFrame)
     [~, ~, ~, rotMatToInertial32] = viewFrame.getOffsetsWrtInertialOrigin(time);
     bodyRotMatFromGlobalInertialToBodyInertial = bodyInfo.bodyRotMatFromGlobalInertialToBodyInertial();
 
-    M33 = rotMatToInertial32' * rotMatToInertial12 * bodyRotMatFromGlobalInertialToBodyInertial * rotz(bodyInfo.surftexturezrotoffset);
+    zRotOffset = bodyInfo.surftexturezrotoffset;
+    rotMatZOffset = [cosd(zRotOffset) -sind(zRotOffset) 0; sind(zRotOffset) cosd(zRotOffset) 0; 0 0 1];
+    
+    M33 = rotMatToInertial32' * rotMatToInertial12 * bodyRotMatFromGlobalInertialToBodyInertial * rotMatZOffset;
     M = eye(4);
     M(1:3,1:3) = M33;    
 end
