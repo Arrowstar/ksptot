604baa984607c72b458af2fcdb0c65efa5a6232b
Arrowstar <hardena@alumni.msoe.edu>
2020-12-15 11:16:02 -0600

    Removed call to bodyRotMatFromGlobalInertialToBodyInertial() in getBodyXformMatrix since this is already handled in the BodyFixedFrame.

--- helper_methods/ksptot_ma/plotting/getBodyXformMatrix.m
+++ helper_methods/ksptot_ma/plotting/getBodyXformMatrix.m
@@ -3,12 +3,11 @@ function M = getBodyXformMatrix(time, bodyInfo, viewFrame)
 
     [~, ~, ~, rotMatToInertial12] = bodyFixedFrame.getOffsetsWrtInertialOrigin(time);
     [~, ~, ~, rotMatToInertial32] = viewFrame.getOffsetsWrtInertialOrigin(time);
-    bodyRotMatFromGlobalInertialToBodyInertial = bodyInfo.bodyRotMatFromGlobalInertialToBodyInertial();
 
     zRotOffset = bodyInfo.surftexturezrotoffset;
     rotMatZOffset = [cosd(zRotOffset) -sind(zRotOffset) 0; sind(zRotOffset) cosd(zRotOffset) 0; 0 0 1];
     
-    M33 = rotMatToInertial32' * rotMatToInertial12 * bodyRotMatFromGlobalInertialToBodyInertial * rotMatZOffset;
+    M33 = rotMatToInertial32' * rotMatToInertial12 * rotMatZOffset;
     M = eye(4);
     M(1:3,1:3) = M33;    
 end
