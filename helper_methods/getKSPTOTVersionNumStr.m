9218e1ee9808c5b8276467c8875e436b7290d6cc
Arrowstar <hardena@alumni.msoe.edu>
2021-10-20 14:43:16 -0500

    Version number to "1.6.9 PR5"

--- helper_methods/getKSPTOTVersionNumStr.m
+++ helper_methods/getKSPTOTVersionNumStr.m
@@ -1,6 +1,6 @@
 function [verNumStr] = getKSPTOTVersionNumStr()
 %getKSPTOTVersionNumStr Provides the current version number of KSPTOT as
 %string for use within various places of the software.
-    verNumStr = '1.6.9 PR4';
+    verNumStr = '1.6.9 PR5';
 end
 
