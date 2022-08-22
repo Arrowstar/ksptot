17fff5a8585a49b6a808c670366ebba763144ed0
Adam Harden <adam.r.harden@nasa.gov>
2022-08-22 15:08:39 -0500

    More performance improvements

--- helper_methods/ksptot_ma/propagation/aerobrake/computeHourAngle.m
+++ helper_methods/ksptot_ma/propagation/aerobrake/computeHourAngle.m
@@ -23,7 +23,7 @@ function hra = computeHourAngle(ut, long, bodyInfo)
     planarRvectEcef = [rECEF(1,:); rECEF(2,:); zeros(1,size(rECEF,2))];
     planarRvectSunEcef = [rVectSunECEF(1,:); rVectSunECEF(2,:); zeros(1,size(rVectSunECEF,2))];
 
-    hra = angleNegPiToPi(dang(planarRvectEcef,planarRvectSunEcef));
+    hra = angleNegPiToPi_mex(dang(planarRvectEcef,planarRvectSunEcef));
     hra(rVectBodyToSunNorm == 0) = 1.0;
     hra = reshape(hra,size(ut));
 end
