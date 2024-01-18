07de2139dd68c57608d49527cf72d884f05248f2
Arrowstar <adam.r.harden@outlook.com>
2024-01-18 14:59:50 -0600

    Set version string to 1.6.10 PR6

--- helper_methods/getKSPTOTVersionNumStr.m
+++ helper_methods/getKSPTOTVersionNumStr.m
@@ -1,6 +1,6 @@
 function [verNumStr] = getKSPTOTVersionNumStr()
 %getKSPTOTVersionNumStr Provides the current version number of KSPTOT as
 %string for use within various places of the software.
-    verNumStr = '1.6.10 PR5';
+    verNumStr = '1.6.10 PR6';
 end
 
